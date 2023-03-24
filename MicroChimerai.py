886a127212b131c6d2c1240540294315f86e7a39
Richard Zaldivar <rmaxz98@gmail.com>
2023-03-23 19:48:37 -0500

    Added tests and seperated function to make it easier to debug

--- MicroChimerai.py
+++ MicroChimerai.py
@@ -7,7 +7,7 @@ from math import sin, cos
 
 class MicroChimerai(MicroCrawler):
     def __init__(self, **kwargs):
-        super().init(**kwargs)
+        super().__init__(**kwargs)
 
         self.agent_count = int(1000) # 3X the length of the lagged_values/input data.
         self.prev_pred_diff = 0
@@ -17,8 +17,29 @@ class MicroChimerai(MicroCrawler):
         self.AV = pd.DataFrame(np.ones((self.agent_count, 1)))
 
     def sample(self, lagged_values, lagged_times=None, name=None, delay=None, **ignored):
-        # First we'll compare lagged values to our predictions of it in AV i.e the first len(lagged_values) elements of AV.
-        diff = self.AV.values[:len(lagged_values)] - lagged_values
+        # First we update the W and L matrices based on if we got the previous prediction correct or not.
+        avg_diff = self.update_WL(lagged_values)
+
+        # We assign all the values from lagged_values to however many slots it takes up in AV.
+        self.AV.loc[:len(lagged_values)-1, 0] = lagged_values
+
+        # Create all matrices needed to calculate the prediction for the next timestep.
+        G = self.W.div(self.W.add(self.L))
+        H = pd.DataFrame(np.ones(self.agent_count, 1))
+        r = uniform(0, 1)
+        queued_connections_row, queued_connections_col = np.where(G<r)
+
+        # Given all the connections between agents we need to use, we create a prediction for the next timestep.
+        prediction = self.create_prediction(queued_connections_row, queued_connections_col, H)
+
+        # We update the previous prediction difference.
+        self.prev_pred_diff = avg_diff
+
+        return prediction
+    
+    def update_WL(self, curr_values):
+        # First we'll compare curr_vales to our predictions of it in AV i.e the first len(lagged_values) elements of AV.
+        diff = self.AV.values[:len(curr_values)] - curr_values
         curr_avg_diff = np.average(diff)
 
         # If we have a better prediction than last time, we consider this a success in terms of our Bayesian updating.
@@ -28,33 +49,26 @@ class MicroChimerai(MicroCrawler):
             self.L = self.L.add(self.P)
         self.P = self.P.sub(self.P)
 
-        # Now we assign all the values from lagged_values to however many slots it takes up in AV.
-        self.AV.loc[:len(lagged_values)-1, 0] = lagged_values
-
-        # Now we create all matrices needed to calculate the prediction for the next timestep.
-        G = self.W.div(self.W.add(self.L))
-        H = pd.DataFrame(np.ones(self.agent_count, 1))
-        r = uniform(0, 1)
-
-        queued_connections_row, queued_connections_col = np.where(G<r)
-
-        # TODO Parallelize this later with each calc queued because they all are just mulitplications to H.
-        for i,j in zip(queued_connections_row, queued_connections_col):
+        return curr_avg_diff
+    
+    def create_prediction(self, queued_rows, queued_cols, H_matrix):
+        for i,j in zip(queued_rows, queued_cols):
             self.P.iat[i,j] = 1
             x_n, y_n = e_conv(self.AV[i].values[0], self.AV[j].values[0])
-            H[i] *= x_n
-            H[j] *= y_n
+            H_matrix[i] *= x_n
+            H_matrix[j] *= y_n
 
-        # Now we calculate the prediction for the next timestep.
-        self.AV = self.AV.mul(H)
-        prediction = self.AV[0].values[0]
+        self.AV = self.AV.mul(H_matrix)
+        curr_prediction = self.AV[0].values[0]
 
-        # We update the previous prediction difference.
-        self.prev_pred_diff = curr_avg_diff
+        return curr_prediction
 
-        return prediction
-
-
+"""
+This function convolves the float values of x and y using rotation matrices to create an operation that can be used to
+improve x and y if the model learns the timing to apply the operation as opposed to improving an operation like other
+models.
+"""
 def e_conv(x, y):
     x_new = x*cos(x*y) - y*sin(x*y)
     y_new = x*sin(x*y) + y*cos(x*y)
+    return x_new, y_new
