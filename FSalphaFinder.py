abb5a4a2d499cd9c83826c53a4e6fe4f07fcd2db
Richard Zaldivar <rmaxz98@gmail.com>
2023-04-28 13:07:11 -0500

    updated alpha finder to improve accuracy

--- FSalphaFinder.py
+++ FSalphaFinder.py
@@ -1,12 +1,12 @@
 import cupy as cp
 import numpy as np
-from scipy.optimize import basinhopping
+import pyswarms as ps
 
 
 def frft(signal, alpha):
     n = len(signal)
     indices = cp.arange(n)
-    M = cp.exp(-1j * cp.pi * alpha * (indices ** 2) / n)
+    M = cp.exp(-1j * cp.pi * cp.float32(alpha) * (indices ** 2) / n)
     return cp.fft.ifft(cp.fft.fft(signal) * M)
 
 
@@ -20,7 +20,7 @@ def cross_correlation(X, Y):
     return cp.abs(cp.sum(cp.conj(X) * Y))
 
 
-def optimize_alpha(signal1_np, signal2_np, initial_alpha):
+def optimize_alpha(signal1_np, signal2_np):
     signal1_gpu = cp.array(signal1_np, dtype=cp.float32)
     signal2_gpu = cp.array(signal2_np, dtype=cp.float32)
 
@@ -29,8 +29,15 @@ def optimize_alpha(signal1_np, signal2_np, initial_alpha):
         Y_alpha = frft(signal2_gpu, alpha)
         return -mehler_kernel(X_alpha, Y_alpha).real
 
-    result = basinhopping(objective_function, initial_alpha, niter=50, stepsize=1)
-    return result.x[0]
+    lb = np.array([0])
+    ub = np.array([2])
+
+    options = {'c1': 0.5, 'c2': 0.3, 'w': 0.9}
+
+    optimizer = ps.single.GlobalBestPSO(n_particles=20, dimensions=1, options=options, bounds=(lb, ub))
+    cost, pos = optimizer.optimize(objective_function, iters=50)
+
+    return pos[0]
 
 
 def generate_test_signals_padded(n, alpha, list_length=225):
@@ -62,16 +69,15 @@ n = 65536
 batch_size = 10
 signals1, signals2, true_alphas = generate_batch_data_padded(batch_size, n)
 
-initial_alpha = np.random.uniform(0, 2)
 estimated_alphas = []
 for i in range(batch_size):
     signal1_np = np.array(signals1[i], dtype=np.float32)
     signal2_np = np.array(signals2[i], dtype=np.float32)
-    estimated_alpha = optimize_alpha(signal1_np, signal2_np, initial_alpha)
+    estimated_alpha = optimize_alpha(signal1_np, signal2_np)
     estimated_alphas.append(estimated_alpha)
 
     print(f"Pair {i + 1}:")
     print("Known alpha:", true_alphas[i])
     print("Estimated alpha:", estimated_alpha)
     print("Absolute error:", abs(true_alphas[i] - estimated_alpha))
-    print()
+
